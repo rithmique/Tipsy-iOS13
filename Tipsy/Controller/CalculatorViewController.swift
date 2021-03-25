13d52f875b9cabdd3ae52cceda77ab7f8ee9a40e
Maxim Vainikka <rithmique@yahoo.com>
2021-03-25 19:05:03 +0100

    probably the end

--- Tipsy/Controller/CalculatorViewController.swift
+++ Tipsy/Controller/CalculatorViewController.swift
@@ -18,6 +18,7 @@ class CalculatorViewController: UIViewController {
     @IBOutlet weak var stepperCountLabel: UILabel!
 
     var tip = 0.0
+    var totalPerPerson = 0.0
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -29,6 +30,7 @@ class CalculatorViewController: UIViewController {
         secondTip.isSelected = false
         thirdTip.isSelected = false
         sender.isSelected = true
+        billLabel.endEditing(true)
         
         let buttonSelected = sender.currentTitle!
         let buttonSelectedMinusPercentage = buttonSelected.dropLast()
@@ -37,9 +39,21 @@ class CalculatorViewController: UIViewController {
 
         }
     @IBAction func stepperChanged(_ sender: UIStepper) {
+        stepperCountLabel.text = String(Int(sender.value))
     }
     @IBAction func calculateButtonPressed(_ sender: UIButton) {
-        print(tip)
+        let checkTotal = Double(billLabel.text!)!
+        let totalTipAmount = checkTotal * tip
+        let total = checkTotal + totalTipAmount
+        totalPerPerson = total / Double(stepperCountLabel.text!)!
+        self.performSegue(withIdentifier: "goToResult", sender: self)
+    }
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        if segue.destination is ResultViewController {
+            let vc = segue.destination as? ResultViewController
+            vc?.resultLabelText = String(format: "%.2f", totalPerPerson)
+            vc?.moneyFromPersonLabelText = "Split between \(stepperCountLabel.text!) people with \(Int(tip * 100))% tip"
+        }
     }
     
 }
--- Tipsy/Controller/ResultViewController.swift
+++ Tipsy/Controller/ResultViewController.swift
@@ -9,27 +9,20 @@
 import UIKit
 
 class ResultViewController: UIViewController {
-
+    
     @IBOutlet weak var resultLabel: UILabel!
     @IBOutlet weak var moneyFromPersonLabel: UILabel!
+    
+    var resultLabelText: String!
+    var moneyFromPersonLabelText: String!
     override func viewDidLoad() {
         super.viewDidLoad()
-
-        // Do any additional setup after loading the view.
+        
+        resultLabel.text = resultLabelText
+        moneyFromPersonLabel.text = moneyFromPersonLabelText
     }
     
     @IBAction func recalculate(_ sender: UIButton) {
         dismiss(animated: true, completion: nil)
     }
-    
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
-    }
-    */
-
 }
